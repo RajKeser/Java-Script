0747a5f35f030c86cfc5150cface9580fce98e3e
Rajkeser <rajkeshar.2024ug2073@iiitranchi.ac.in>
2025-11-11 19:57:11 +0530

    Added Data_types.js

---
+++ Data_types.js
@@ -0,0 +1,20 @@
+"use strict" // treat all the code as modern version of javascript
+
+//  alert("Welcome to Data Types in JavaScript"); // we are using nodejs so alert will not work here it will work in browser
+ 
+// ECMA Script -> https://262.ecma-international.org/15.0/index.
+
+let name="Rajkeshar Yadav";
+let age=20;
+let isApproved=true;
+
+// number range: -2^53 to 2^53
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique identifier
+
+// object => to store key value pairs
+
+console.log(typeof name);
+console.log(typeof null); // object (this is a bug in javascript)
+console.log(typeof undefined); // undefined
